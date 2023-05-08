b1dce98658e973ebceb6b07f56ea818a9c4024ad
turing0 <turing175@gmail.com>
2023-05-08 17:32:30 +0800

    update channel

--- app/channel/page.tsx
+++ app/channel/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Head from 'next/head';
+import { metadata } from "../layout";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
@@ -30,11 +31,12 @@ const socials = [
 ];
 
 export default function Example() {
+    const pageTitle = "Channel";
+
 	return (
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Head>
-                <title>Channel</title>
-                <meta property="og:title" content="Channel" key="title" />
+                <title>{metadata.title.template.replace("%s", pageTitle)}</title>
             </Head>
             <Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
