c22550d15c10691283715a717b083216940301fc
turing0 <turing175@gmail.com>
2023-05-08 22:51:48 +0800

    update title

--- app/channel/page.tsx
+++ app/channel/page.tsx
@@ -36,9 +36,6 @@ export default function Example() {
 	return (
 		<RootLayout pageTitle={pageTitle}>
 			<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
-				<Head>
-					<title>{metadata.title.template.replace("%s", pageTitle)}</title>
-				</Head>
 				<Navigation />
 				<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
 					<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
