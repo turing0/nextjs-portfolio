41f60f4dec00a0b7b28d22fcfd82e3f6a909ae40
turing0 <turing175@gmail.com>
2023-05-08 16:37:02 +0800

    update channel

--- app/channel/page.tsx
+++ app/channel/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
+const pageTitle = "Channel";
+
 const socials = [
 	{
 		// icon: <Twitter size={20} />,
@@ -31,7 +33,7 @@ export default function Example() {
 	return (
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Head>
-                <title>Channel</title>
+                <title>{pageTitle}</title>
             </Head>
             <Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
