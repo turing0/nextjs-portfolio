16592f0ff612512050bceb62a4f9ce5a595d5098
turing0 <turing175@gmail.com>
2023-05-08 17:15:14 +0800

    update channel

--- app/channel/page.tsx
+++ app/channel/page.tsx
@@ -34,6 +34,7 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Head>
                 <title>Channel</title>
+                <meta property="og:title" content="Channel" key="title" />
             </Head>
             <Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
