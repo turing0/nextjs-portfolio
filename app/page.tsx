4cde77e2de127e5c960524413244fff7b89f91c6
turing0 <turing175@gmail.com>
2025-03-06 19:39:49 +0800

    update lin

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 const navigation = [
 	{ name: "Home", href: "https://laphel.com" },
+	{ name: "Blog", href: "https://blog.laphel.com" },
 	{ name: "Projects", href: "/projects" },
 	{ name: "Channel", href: "/channel" },
 	{ name: "Contact", href: "/contact" },
