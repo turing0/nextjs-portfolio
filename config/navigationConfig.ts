f05a44d5b3f760dd9d2e1286db3907e63adaf459
turing0 <turing175@gmail.com>
2025-03-07 21:56:00 +0800

    update

--- config/navigationConfig.ts
+++ config/navigationConfig.ts
@@ -1,6 +1,6 @@
 export const navigationLinks = [
     { name: "Home", href: "https://laphel.com" },
-    { name: "Blog", href: "https://blog.laphel.com" },
+    // { name: "Blog", href: "https://blog.laphel.com" },
     { name: "Projects", href: "/projects" },
     { name: "Channel", href: "/channel" },
     { name: "Contact", href: "/contact" },
